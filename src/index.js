367617599fc7df4f51cc4e9082ad4e4436bbead6
AlenaSazonova <alenka03.92@mail.ru>
2022-08-10 20:02:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let x = 0; x < bracketsConfig.length; x++) {
+      if ((str[i] == bracketsConfig[x][1]) && (arr[arr.length - 1] ==  bracketsConfig[x][0])) {
+        arr.pop(arr.length - 1);
+      } else if (bracketsConfig[x][0] == str[i]) {
+        arr.push(str[i]);
+      }
+    }    
+  }
+  return arr.length == 0;
 }
